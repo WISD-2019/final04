0dd76471ede350995fec681568772651edaf7b34
lai-chun-lin <axd5201211@gmail.com>
2019-12-30 11:19:48 +0800

    首頁

---
+++ resources/views/page.blade.php
@@ -0,0 +1,118 @@
+@extends('layouts.template')
+
+@section('title', 'Page Title')
+
+@section('content')
+<H1>Los Angeles</H1>
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+    <li data-target="#demo" data-slide-to="3"></li>
+    <li data-target="#demo" data-slide-to="4"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+    <a href="/"><img src="https://image.winudf.com/v2/image/Y29tLmhkd2Nhci50aWFua29uZ2JpemhpX3NjcmVlbl8xMV8xNTI2MDE5MDY1XzAxNg/screen-11.jpg?fakeurl=1&type=.jpg" alt="Los Angeles" width="1200" height="500"></a>
+      <div class="carousel-caption">
+        <h3>Los Angeles</h3>
+        <p>We had such a great time in LA!</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./image/1.jpg" alt="Chicago" width="1200" height="500" >
+      <div class="carousel-caption">
+        <h3>Chicago</h3>
+        <p>Thank you, Chicago!</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./image/2.jpg" alt="New York" width="1200" height="500">
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>We love the Big Apple!</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://image.winudf.com/v2/image/Y29tLmhkd2Nhci50aWFua29uZ2JpemhpX3NjcmVlbl8xMV8xNTI2MDE5MDY1XzAxNg/screen-11.jpg?fakeurl=1&type=.jpg" alt="" width="1200" height="500">
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>We love the Big Apple!</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./image/2.jpg" alt="New York" width="1200" height="500">
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>We love the Big Apple!</p>
+      </div>
+    </div>
+
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+<H1></H1>
+
+<nav aria-label="Page navigation example">
+
+<div class="row">
+    <div class="col-lg-6">
+        <div class="thumbnail">
+
+            <div class="caption">
+              <H2>版本更新公告</H2>
+              <ul>
+                <li>Lorem ipsum donec id elit non mi porta gravida at eget metus.</li>
+                <li>Lorem ipsum donec id elit non mi porta gravida at eget metus.</li>
+                <li>Lorem ipsum donec id elit non mi porta gravida at eget metus.</li>
+                <li>Lorem ipsum donec id elit non mi porta gravida at eget metus.</li>
+                <li>Lorem ipsum donec id elit non mi porta gravida at eget metus.</li>
+                <li>Lorem ipsum donec id elit non mi porta gravida at eget metus.</li>
+              </ul>
+            </div>
+
+        </div>
+      </div>
+  <div class="col-lg-6">
+    <div class="thumbnail">
+        <div class="caption">
+          <H2>最新消息</H2>
+            <ul class="nav nav-tabs">
+              <li class="active"><a class="nav-link" data-toggle="tab" href="#home">Home</a></li>
+              <li><a class="nav-link" data-toggle="tab" href="#menu1">頁籤 1</a></li>
+              <li><a class="nav-link" data-toggle="tab" href="#menu2">頁籤 2</a></li>
+              <li><a class="nav-link" data-toggle="tab" href="#menu3">頁籤 3</a></li>
+            </ul>
+        <div class="tab-content">
+            <div id="home" class="tab-pane active">
+              <p>至今刪除將來時間奇蹟並且儘量最佳台北，一樣高中最快上了一年積，房地產大家都本來確保原來評分完善對話隊，鈴聲醫藥之中活力人生收購意思授權方式，室內導致系統快樂不怕內容簡介收藏本頁，下面統計大部分學會有時候必須食品白色實在支撐那時充分一看歌。</p>
+            </div>
+            <div id="menu1" class="tab-pane fade">
+              <p>夢想躺在他就滿足獨立區別貢獻會員有關促進民主是指三，監督大廈提到存儲各種韓國好看承認基本上鄭州那時反正，士兵依然損失廠商聰明取得資源意味着不少什麼要求斑竹留言幸福，追求像是東方法人每日問題經濟無限複製有着不喜歡我就期待共和國，年度此刻有可能對於航空動作深深奇，多少一定需要方案平方米競爭右鍵事故玻璃都，最後傷害處於正文站在電器相。</p>
+            </div>
+            <div id="menu2" class="tab-pane fade">
+              <p>接下來發揮描述看著眾多經驗今後貿易動畫在此審核命運外資努力利，停止想到不過熱情精華一眼懷疑參與放棄提高多麼符合發帖連結來到，能不能我們印度就有人大常委會看著一片邀請不，團體活力去了研發倒是新手供求軟件試題，至少無論公主物質後果經典連結一定大盤一台這，漢化正式學習每一個當你曝光。</p>
+            </div>
+            <div id="menu3" class="tab-pane fade">
+              <p>相對一點帶來樂隊房間那天以前大盤效率感覺到東莞部門家電，種類污染說明事實上放心當時性質同一我，除了於是是指組合搜索追求收入也許都，機構三星緩緩主流幾乎人員優惠少女評估一把字幕，協助設為不需要聯賽網頁那些因而論壇此處老。</p>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@stop
