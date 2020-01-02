9184e61299eebff6e070237ebbe2091ea00846e3
3A632083 <kiki87119@gmail.com>
2020-01-02 19:09:37 +0800

    建立 Leave model

---
+++ app/Leave.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Leave extends Model
+{
+    protected $fillable = [
+        'user_id', 'type', 'reason', 'stauts', 'prove', 'apply_time', 'start_time', 'end_time',
+
+    ];
+}
