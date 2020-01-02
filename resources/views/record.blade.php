b8429435214df5632cf18d186086634bdf853223
lai-chun-lin <axd5201211@gmail.com>
2020-01-03 00:57:19 +0800

    請假記錄查詢視圖

--- resources/views/record.blade.php
+++ resources/views/record.blade.php
@@ -1,7 +1,35 @@
 @extends('layouts.template')
 
-@section('title', '請假出差')
+@section('title', '請假紀錄查詢')
 
 @section('content')
-
-{{dd($data)}}
+<div class="row">
+    <div class="col-lg">
+        <div class="thumbnail">
+            <div class="caption">
+                <H2>請假紀錄查詢</H2>
+                <table class="table table-hover">
+                    <thead>
+                    <tr>
+                        <th>{{Auth::user()->name}}</th>
+                        <th>假別</th>
+                        <th>理由</th>
+                        <th>審核狀況</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($query as $data)
+                    <tr>
+                        <td></td>
+                        <td>{{$data->type}}</td>
+                        <td>{{$data->reason}}</td>
+                        <td>{{$data->stauts}}</td>
+                    </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+@stop
