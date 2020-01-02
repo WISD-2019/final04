e28d1455d4df2de47992069cbe3fcb4b6a332b9f
lai-chun-lin <axd5201211@gmail.com>
2020-01-03 00:51:09 +0800

    在navbar的選單內加入 紀錄查詢項目

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -29,7 +29,9 @@
                     </a>
 
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    
+                        <a class="dropdown-item" href="{{ route('record') }}">
+                            <span class="fa fa-sign-out"></span>紀錄查詢
+                        </a>
                         <a class="dropdown-item" href="{{ route('logout') }}"
                             onclick="event.preventDefault();
                                             document.getElementById('logout-form').submit();"><span  class="fa fa-sign-out"></span>
