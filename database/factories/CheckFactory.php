647f75cb6ae761ffb28f4730f25aa988f501c6be
3A632083 <kiki87119@gmail.com>
2020-01-04 22:36:24 +0800

    錯誤修正

--- database/factories/CheckFactory.php
+++ database/factories/CheckFactory.php
@@ -2,17 +2,12 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-<<<<<<< HEAD
+
 use App\check;
 use Faker\Generator as Faker;
 
 $factory->define(check::class, function (Faker $faker) {
-=======
-use App\Check;
-use Faker\Generator as Faker;
 
-$factory->define(Check::class, function (Faker $faker) {
->>>>>>> c2cf393d8a8ac93a867722252110659de0f5a6d8
     return [
         //
     ];
