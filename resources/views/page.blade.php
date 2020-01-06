8058baf1131956e84317b70dde0a002192c7f1dc
lai-chun-lin <axd5201211@gmail.com>
2020-01-06 21:05:05 +0800

    製作首頁視圖

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -3,47 +3,31 @@
 @section('title', 'Page Title')
 
 @section('content')
-<H1>Los Angeles</H1>
+<H1>企業員工出差線上申請</H1>
 
 <div id="demo" class="carousel slide" data-ride="carousel">
   <ul class="carousel-indicators">
     <li data-target="#demo" data-slide-to="0" class="active"></li>
     <li data-target="#demo" data-slide-to="1"></li>
     <li data-target="#demo" data-slide-to="2"></li>
-    <li data-target="#demo" data-slide-to="3"></li>
-    <li data-target="#demo" data-slide-to="4"></li>
   </ul>
   <div class="carousel-inner">
     <div class="carousel-item active">
-    <a href="/"><img src="https://image.winudf.com/v2/image/Y29tLmhkd2Nhci50aWFua29uZ2JpemhpX3NjcmVlbl8xMV8xNTI2MDE5MDY1XzAxNg/screen-11.jpg?fakeurl=1&type=.jpg" alt="Los Angeles" width="1200" height="500"></a>
+    <a href="/"><img src="https://images2.alphacoders.com/499/thumb-1920-499590.jpg" alt="Los Angeles" width="100%" height="400"></a>
       <div class="carousel-caption">
         <h3>Los Angeles</h3>
         <p>We had such a great time in LA!</p>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="./image/1.jpg" alt="Chicago" width="1200" height="500" >
+      <img src="https://cdn.hipwallpaper.com/i/35/48/NQPLiF.jpg" alt="Chicago" width="100%" height="400" >
       <div class="carousel-caption">
         <h3>Chicago</h3>
         <p>Thank you, Chicago!</p>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="./image/2.jpg" alt="New York" width="1200" height="500">
-      <div class="carousel-caption">
-        <h3>New York</h3>
-        <p>We love the Big Apple!</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="https://image.winudf.com/v2/image/Y29tLmhkd2Nhci50aWFua29uZ2JpemhpX3NjcmVlbl8xMV8xNTI2MDE5MDY1XzAxNg/screen-11.jpg?fakeurl=1&type=.jpg" alt="" width="1200" height="500">
-      <div class="carousel-caption">
-        <h3>New York</h3>
-        <p>We love the Big Apple!</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="./image/2.jpg" alt="New York" width="1200" height="500">
+      <img src="https://areajugones.sport.es/wp-content/uploads/2019/11/Studio-Ghibli-780x440.jpg" alt="New York" width="100%" height="400">
       <div class="carousel-caption">
         <h3>New York</h3>
         <p>We love the Big Apple!</p>
@@ -58,9 +42,35 @@
     <span class="carousel-control-next-icon"></span>
   </a>
 </div>
-<H1></H1>
 
-<nav aria-label="Page navigation example">
+<section class="box-content container box-1">
+			<div class="row">
+				<div class="col-sm-4 ">
+					<div class="service">
+						<a href="#"><img src="images/icon1.png" title="icon-name"></a>
+						<h3>The largest network of laboratory centers</h3>
+						<p>We have contracts with laboratories all over the world to provide lab services at the best prices. We also contract with the free-standing independent laboratories for... </p>
+						<a class="btn btn-2 btn-sm" href="#">Read More</a>
+					</div>
+				</div>
+				<div class="col-sm-4 ">
+					<div class="service">
+						<a href="#"><img src="images/icon3.png" title="icon-name"></a>
+						<h3>The modern laboratory facilities</h3>
+						<p>We’ve done our research to ensure that the medical laboratory equipment we use has been industry approved and has surpassed statistical benchmarks for risk assessment. Our team members are highly trained...</p>
+						<a class="btn btn-2 btn-sm" href="#">Read More</a>
+					</div>
+				</div>
+				<div class="col-sm-4 ">
+					<div class="service">
+						<a href="#"><img src="images/icon2.png" title="icon-name"></a>
+						<h3>Helpful test tips</h3>
+						<p>If you are nervous or have a tendency to feel woozy or faint, tell the phlebotomist before you begin. Your blood can be drawn while you are lying down, which will help you avoid fainting and injuring yourself. If, at any time, you feel faint or lightheaded... </p>
+						<a class="btn btn-2 btn-sm" href="#">Read More</a>
+					</div>
+				</div>		
+			</div>
+		</section>
 
 <div class="row">
     <div class="col-lg-6">
@@ -107,4 +117,6 @@
       </div>
     </div>
   </div>
+
+  		
 @stop
