859a1f98563a1558097204d2a2878cc236242f94
lai-chun-lin <axd5201211@gmail.com>
2020-01-06 21:04:53 +0800

    asset style樣式

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -11,6 +11,7 @@
     @show
 
     <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
