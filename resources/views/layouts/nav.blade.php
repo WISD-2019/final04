16734dd6f65ddc79b8c68576d123ed4da0691e52
3A632083 <kiki87119@gmail.com>
2020-01-08 00:07:26 +0800

    沒有修改

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -25,7 +25,7 @@
             @if (Auth::check())
                 <li class="nav-item dropdown">
                     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        <span class="fa fa-user-o"></span>{{ Auth::user()->name }} 
+                        <span class="fa fa-user-o"></span>{{ Auth::user()->name }}
                     </a>
 
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
