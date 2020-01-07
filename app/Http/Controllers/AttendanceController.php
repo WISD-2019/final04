1b4022de35ca536d35ebef761f94be663918e0e4
3A632097 <a0981792089@gmail.com>
2020-01-07 21:50:17 +0800

    報表--抓取打卡(works)、請假(leave)、出差(travels)、人員(users)資料表的資料

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers;
 use App\Works;
 use App\User;
 use App\Leave;
+use App\Travel;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class AttendanceController extends Controller
 {
     public function attend()
-    {        
+    {       
+        //抓取打卡資料表，並對應到登入的使用者
+        $query=Works::where('user_id',Auth::user()->id)->get();
+        //給一個空的陣列，搜尋打卡資料表中的on_work，把重複的資料過濾掉
         $result=array();
         $work = Works::select('on_work')->get();
         foreach($work as $works)
@@ -18,8 +22,12 @@ class AttendanceController extends Controller
             array_push($result,substr($works->on_work,0,10));
         }
         $on_work=array_unique($result);
-        $query=Works::where('user_id',Auth::user()->id)->get();
-        $leave = Leave::all();
-        return View('report',['query'=>$query,'leave'=>$leave,'on_work'=>$on_work]);
+
+        //抓取請假資料表，並對應到登入的使用者
+        $leave = Leave::where('user_id',Auth::user()->id)->get();
+        //抓取出差資料表，並對應到登入的使用者
+        $travel = Travel::where('user_id',Auth::user()->id)->get();
+        
+        return View('report',['query'=>$query,'leave'=>$leave,'on_work'=>$on_work,'travel'=>$travel]);
     }
 }
